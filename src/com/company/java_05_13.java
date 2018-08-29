5d68ee96ba0bed69c44cc830058b66c133282496
firedevelop <firedevelop.com@gmail.com>
2018-08-29 10:01:30 +0200

    .

--- src/com/company/java_05_13.java
+++ src/com/company/java_05_13.java
@@ -1,10 +1,20 @@
 package com.company;
 
-import java.util.Comparator;
+import java.util.Arrays;
 
 public class java_05_13 {
-    public static void main(String[] args){
+        public static void main(String[]args){
+        int[] matrix ={3, 15, 2, 6, 1};
+            System.out.println(Arrays.toString(matrix));
+            int total = functionMaxValue(matrix);
+            System.out.println("The max value is: " + total);
 
+        }
+
+        private static int functionMaxValue(int[] matrix){
+            Arrays.sort(matrix);
+            int maxMatrix = matrix[matrix.length-1];
+
+            return maxMatrix;
     }
-
-}
+    }
---
+++ src/com/company/java_05_15.java
@@ -0,0 +1,8 @@
+package com.company;
+        import java.io.IOException;
+        import java.util.ArrayList;
+        import java.util.concurrent.ThreadLocalRandom;
+
+public class java_05_15 {
+    public static void main
+}
