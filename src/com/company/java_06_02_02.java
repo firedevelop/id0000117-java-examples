bcf62b6a210e1434117ef9484618ac0541b70c2c
OdooPlay <root@MacBook-Pro-de-Marina.local>
2018-09-07 12:27:15 +0200

    setup Odoo Play

--- src/com/company/java_06_02_02.java
+++ src/com/company/java_06_02_02.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class java_06_02_02 {
     public static void main(String[] args){
-        System.out.println("hi odoo");
+        System.out.println("hi odoo Play");
 
     }
   }
