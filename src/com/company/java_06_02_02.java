ebcf7a665e3ba458a27294f4387c12ba997bad95
System Administrator <root@MacBook-Pro-de-Marina.local>
2018-09-07 12:22:36 +0200

    .

---
+++ src/com/company/java_06_02_02.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class java_06_02_02 {
+    public static void main(String[] args){
+        System.out.println("hi odoo");
+
+    }
+  }
+}
