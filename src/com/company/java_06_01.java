67b4493abd79fd3a50c1f05d22f0eea95c1245ca
firedevelop <firedevelop.com@gmail.com>
2018-09-14 10:44:17 +0200

    fire

---
+++ src/com/company/java_06_01.java
@@ -0,0 +1,42 @@
+package com.company;
+public class java_06_01 {
+    public static void main(String[] args){
+/*
+isDigit
+isLetter
+isLetterOrDigit
+isLowerCase
+isUpperCase
+isSpaceChar
+isWhiteSpace
+toLowerCase
+toUpperCase
+toString
+forDigit
+valueOf
+equals
+equalsIgnoreCase
+regionMatches
+substring
+trim
+compareTo
+ToIgnoreCase
+concat
+charAt
+length
+indexOf
+lastIndexOf
+isEmpty
+contains
+startsWith
+endsWith
+toLowerCase
+toUpperCase
+replace
+toCharArray
+valueOf(char, 0,3)
+getChars
+  */
+        System.out.println(2661);
+    }
+}
