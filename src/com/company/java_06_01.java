fb47da6d0136a8232762778f1a167d6e2ebbe804
firedevelop <firedevelop.com@gmail.com>
2018-09-14 10:25:34 +0200

    resolver merge

---
+++ src/com/company/java_06_01.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+/*
+valueOf
+equals
+equalsIgnoreCase
+regionMatches
+compareTo
+comparteToIgnoreCase
+concat
+substring
+trim
+*/
+public class java_06_01 {
+    public static void main(String[]args){
+       char a= 2000;
+        String text = "hola ";
+
+        System.out.println(String.valueOf(a));
+    }
+}
+
