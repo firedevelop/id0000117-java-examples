8426191103ac94c755f0f109de69d11d200de7bd
firedevelop <firedevelop.com@gmail.com>
2018-09-15 16:51:14 +0200

    .fire

--- src/com/company/java_06_04.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class java_06_04 {
-    public static void main(String[]args) {
-        System.out.println("he");
-    }
-}
--- src/com/company/java_06_04_IMPORTANT_Chars.java
+++ src/com/company/java_06_04_IMPORTANT_Chars.java
@@ -1,6 +1,16 @@
 package com.company;
-public class java_06_01 {
-    public static void main(String[] args){
+public class java_06_04_IMPORTANT_Chars {
+    public static void main(String[]args) {
+        String s1 = "Buda Shakyamuni";
+        char c[] = s1.toCharArray();
+        System.out.println(Character.);
+
+        }}
+
+
+
+
+
 /*
 isDigit
 isLetter
@@ -9,34 +19,31 @@ isLowerCase
 isUpperCase
 isSpaceChar
 isWhiteSpace
-toLowerCaseº
+toLowerCase
 toUpperCase
 toString
-forDigit
+
 valueOf
 equals
 equalsIgnoreCase
 regionMatches
+compareTo
+charAt
 substring
 trim
-compareTo
-ToIgnoreCase
-concat
-charAt
 length
 indexOf
 lastIndexOf
 isEmpty
-contains
 startsWith
 endsWith
-toLowerCase
-toUpperCase
 replace
+
+ToIgnoreCase
+concat
+contains
 toCharArray
 valueOf(char, 0,3)
 getChars
+forDigit
   */
-        System.out.println(2661);
-    }
-}
