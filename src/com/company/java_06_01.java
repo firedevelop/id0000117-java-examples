8ca2d77592b3cdfcc27eecef2c39e2eee2486b05
firedevelop <firedevelop.com@gmail.com>
2018-09-16 12:34:12 +0200

    .

--- src/com/company/java_06_01.java
+++ src/com/company/java_06_01.java
@@ -1,7 +1,40 @@
 package com.company;
+
+import java.util.Scanner;
+
 public class java_06_01 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Intro word1: ");
+        String s1 = sc1.next();
+        System.out.println(s1);
+        System.out.print("intro word2: ");
+        String s2 = sc1.next();
+        System.out.println(s2);
+        System.out.println("s1.length = " + s1.length());
+        if (s1.length() > s2.length()) {
+            System.out.println("The low number is: " + s2);
+        }
+        if (s1.length() < s2.length()) {
+            System.out.println("The low number is:" + s1);
+        } else {
+            System.out.println("the numbers are equals");
+
+        }
+    }
+}
+
+
+
+
 /*
+        ArrayList<Integer> b=new ArrayList<Integer>();
+
+        String Str1 = new String("Welcome to Tutorialspoint.com");
+        String[] Str2 = new String[7];
+            Str1.getChars(2, 9, Str2, 0);
+            System.out.println(Str2 );
+
 isDigit
 isLetter
 isLetterOrDigit
@@ -9,34 +42,31 @@ isLowerCase
 isUpperCase
 isSpaceChar
 isWhiteSpace
-toLowerCaseº
+toLowerCase
 toUpperCase
 toString
-forDigit
+
 valueOf
 equals
 equalsIgnoreCase
 regionMatches
+compareTo
+charAt
 substring
 trim
-compareTo
-ToIgnoreCase
-concat
-charAt
 length
 indexOf
 lastIndexOf
 isEmpty
-contains
 startsWith
 endsWith
-toLowerCase
-toUpperCase
 replace
+
+toIgnoreCase
+concat
+contains
 toCharArray
 valueOf(char, 0,3)
 getChars
+forDigit
   */
-        System.out.println(2661);
-    }
-}
--- src/com/company/java_06_04_IMPORTANT_Chars.java
+++ src/com/company/java_06_04_IMPORTANT_Chars.java
@@ -1,17 +1,41 @@
 package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class java_06_04_IMPORTANT_Chars {
-    public static void main(String[]args) {
-        String s1 = "Buda Shakyamuni";
-        char c[] = s1.toCharArray();
-        System.out.println(Character.);
+    public static void main(String[] args) {
+        Scanner sc1 = new Scanner(System.in);
+        System.out.print("Intro word1: ");
+        String s1 = sc1.next();
+        System.out.println(s1);
+        System.out.print("intro word2: ");
+        String s2 = sc1.next();
+        System.out.println(s2);
+        System.out.println("s1.length = " + s1.length());
+        if (s1.length() > s2.length()) {
+            System.out.println("The low number is: " + s2);
+        }
+        if (s1.length() < s2.length()) {
+            System.out.println("The low number is:" + s1);
+        } else {
+            System.out.println("the numbers are equals");
 
-        }}
-
+        }
+    }
+}
 
 
 
 
 /*
+        ArrayList<Integer> b=new ArrayList<Integer>();
+
+        String Str1 = new String("Welcome to Tutorialspoint.com");
+        String[] Str2 = new String[7];
+            Str1.getChars(2, 9, Str2, 0);
+            System.out.println(Str2 );
+
 isDigit
 isLetter
 isLetterOrDigit
@@ -39,7 +63,7 @@ startsWith
 endsWith
 replace
 
-ToIgnoreCase
+toIgnoreCase
 concat
 contains
 toCharArray
