8591c8f5807216ef493c4374642b02593138c7d6
OdooPlay <root@MacBook-Pro-de-Marina.local>
2018-09-17 20:50:15 +0200

    .

--- src/com/company/delete.java
+++ src/com/company/delete.java
@@ -6,7 +6,5 @@ public class delete {
        String s1 ="buda shakyamuni";
        char[] c2 = {'a','b'};
        char[] c1 = new char[s1.length()];
-       Character.forDigit(0,5,s1,0);
-
     }
 }
